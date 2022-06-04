7486fbc2422eda4aec9dc89129fdfeee428b2a0b
Mohamed Achaq <motriyanox@gmail.com>
2022-06-04 08:34:29 +0100

    adding a footer

---
+++ components/ui/Footer.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { SiTwitter } from "react-icons/si";
+import { IconButton } from "./Buttons";
+
+const Footer = () => {
+  return (
+    <footer className="w-full mt-48 pb-8 flex px-12 py-4 justify-between items-center">
+      <div className="flex w-full justify-center items-start flex-col gap-2">
+        <h1 className="text-2xl font-bold text-black dark:text-white">Plana</h1>
+        <h2 className="text-xl text-black dark:text-white">
+          Take control over your life !
+        </h2>
+        <p className="text-lg text-black dark:text-white">
+          made by{" "}
+          <a
+            className="text-zinc-700 dark:text-zinc-300"
+            href="https://twitter.com/ac__haq"
+          >
+            @ac__haq
+          </a>
+        </p>
+      </div>
+      <Link href="https://twitter.com/ac__haq" passHref>
+        <a>
+          <IconButton icon={<SiTwitter />} />
+        </a>
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;
