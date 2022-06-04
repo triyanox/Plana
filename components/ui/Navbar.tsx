2a620b2a4b191ea3ff39089b87f42ca44424ad1c
Mohamed Achaq <motriyanox@gmail.com>
2022-06-04 08:35:26 +0100

    adding a navbar

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,23 @@
+import { FilledButton } from "./Buttons";
+import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex px-12 py-4 justify-between items-center">
+      <Link href="/" passHref>
+        <a className="text-2xl font-bold text-black dark:text-white">Plana</a>
+      </Link>
+      <div className="flex justify-center items gap-4">
+        <Link href="/login" passHref>
+          <a>
+            <FilledButton text="Login" />
+          </a>
+        </Link>
+        <ThemeToggle />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
