a252bfa9b239e650ed3587b5b261269cac744077
Mohamed Achaq <motriyanox@gmail.com>
2022-06-04 08:33:59 +0100

    new signuppage component

---
+++ components/layout/SignupPage.tsx
@@ -0,0 +1,12 @@
+import Signup from "../ui/Signup";
+import Main from "./Main";
+
+const SignupPage = () => {
+  return (
+    <Main>
+      <Signup />
+    </Main>
+  );
+};
+
+export default SignupPage;
