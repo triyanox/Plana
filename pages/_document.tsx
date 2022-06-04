c03e265a060dafd3edfe5893ebc57f32b0bd183b
Mohamed Achaq <motriyanox@gmail.com>
2022-06-04 08:37:26 +0100

    custom document component

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head></Head>
+      <body className=" scroll-smooth antialiased  ">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
