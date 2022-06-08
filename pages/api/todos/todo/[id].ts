e87e84632dcfd91f2a88713eb07346cb1fe936e6
Mohamed Achaq <motriyanox@gmail.com>
2022-06-08 04:14:38 +0100

    dynamic routing for todos

---
+++ pages/api/todos/todo/[id].ts
@@ -0,0 +1,39 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../lib/prisma";
+import jwt from "jsonwebtoken";
+import { env } from "process";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "PUT") {
+    const token = req.cookies.token;
+    if (!token)
+      return res.status(401).send("Access denied. No token provided.");
+    const user: any = jwt.verify(token, env.JWT_SECRET as string);
+    if (!user) return res.status(401).send("Access denied.");
+    const isExist = await prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+    if (!isExist) {
+      return res.status(400).json({ error: "user not found" });
+    }
+    const { id } = req.query;
+    const { status } = req.body;
+    const todo = await prisma.todo.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        status,
+      },
+    });
+    if (!todo) {
+      return res.status(400).json({ error: "todo not found" });
+    }
+    res.status(200).json(todo);
+  }
+}
