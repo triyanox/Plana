3cdbca0672c228619ea44c09928310fe1b29c31f
Mohamed Achaq <motriyanox@gmail.com>
2022-06-08 04:12:06 +0100

    New Todo component

---
+++ components/ui/Todo.tsx
@@ -0,0 +1,65 @@
+import { todo } from "../hooks/List";
+import { FiCheck } from "react-icons/fi";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Status } from "@prisma/client";
+import * as Todo from "../../lib/todo";
+
+const TodoCard = (props: { todo: todo }) => {
+  const [todo, setTodo] = useState(props.todo);
+  const handelUpdate = async (e: any) => {
+    try {
+      const todoData = {
+        id: todo.id,
+        status: todo.status === Status.DONE ? Status.OPEN : Status.DONE,
+      };
+      const res = await Todo.update(todoData);
+      setTodo(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <motion.div
+      key={todo.id}
+      initial={{ opacity: 0, y: -20, scale: 0.8 }}
+      animate={{ opacity: 1, y: 0, scale: 1 }}
+      exit={{ opacity: 0, y: -20, scale: 0.8 }}
+      transition={{ duration: 0.5, delay: 0.2 }}
+      className="flex w-full justify-start gap-4 items-center px-8 py-3 my-4
+            bg-zinc-100 rounded-xl dark:bg-zinc-800"
+    >
+      <div
+        onClick={handelUpdate}
+        className="w-8 h-8 cursor-pointer flex justify-center items-center bg-zinc-300 dark:bg-zinc-600 rounded-lg"
+      >
+        <AnimatePresence exitBeforeEnter>
+          {todo.status === Status.DONE && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.4 }}
+            >
+              <FiCheck className="text-2xl" />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+      <div
+        className="flex w-full justify-between items-center  
+            "
+      >
+        <p
+          className={`text-lg ${
+            todo.status === Status.DONE && "line-through "
+          } font-semibold text-left decoration-2  text-zinc-700 dark:text-zinc-300 transition-all duration-500`}
+        >
+          {todo.text}
+        </p>
+      </div>
+    </motion.div>
+  );
+};
+
+export default TodoCard;
