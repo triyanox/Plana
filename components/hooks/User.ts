facbb4d1412f6423fb83f2fe0709255406322fae
Mohamed Achaq <motriyanox@gmail.com>
2022-06-08 04:09:38 +0100

    Used Hook Updated

--- components/hooks/User.ts
+++ components/hooks/User.ts
@@ -1,10 +1,34 @@
 import { createContext, useContext } from "react";
 
 export const UserContext = createContext({
-  id: 0,
-  name: "",
-  email: "",
-  loggedIn: false,
+  user: {
+    id: 0,
+    name: "",
+    email: "",
+    lists: [
+      {
+        id: 0,
+        name: "",
+        userId: 0,
+        createdAt: "",
+        updatedAt: "",
+      },
+    ],
+    loggedIn: false,
+  },
+  setUser: (user: {
+    id: number;
+    name: string;
+    email: string;
+    lists: {
+      id: number;
+      name: string;
+      userId: number;
+      createdAt: string;
+      updatedAt: string;
+    }[];
+    loggedIn: boolean;
+  }) => {},
 });
 
 export const useUser = () => {
