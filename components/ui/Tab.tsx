e91067704a382f042c7518a6be17604a2d31e704
Mohamed Achaq <motriyanox@gmail.com>
2022-06-08 04:11:42 +0100

    Tab added

--- components/ui/Tab.tsx
+++ components/ui/Tab.tsx
@@ -3,11 +3,15 @@ import { ReactNode } from "react";
 type TabProps = {
   icon: ReactNode;
   text: string;
+  onClick?: () => void;
 };
 
 const Tab = (props: TabProps) => {
   return (
-    <button className="flex gap-4 transition-all duration-700 hover:bg-zinc-300 hover:dark:bg-zinc-700 text-black dark:text-white justify-start px-4 items-center w-full py-2 rounded-xl ">
+    <button
+      onClick={props.onClick}
+      className="flex gap-4   transition-all duration-700 hover:bg-zinc-200 hover:dark:bg-zinc-700 text-black dark:text-white justify-start px-4 items-center w-full py-2 rounded-xl "
+    >
       {props.icon}
       <span className=" text-2xl">{props.text}</span>
     </button>
@@ -15,3 +19,15 @@ const Tab = (props: TabProps) => {
 };
 
 export default Tab;
+
+export const TabSm = (props: TabProps) => {
+  return (
+    <button
+      onClick={props.onClick}
+      className="flex gap-2 transition-all duration-700 hover:bg-zinc-100 hover:dark:bg-zinc-900 text-black dark:text-white justify-start px-4 items-center w-full py-2 rounded-xl "
+    >
+      {props.icon}
+      <span className=" text-lg">{props.text}</span>
+    </button>
+  );
+};
