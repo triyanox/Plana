8f6d152410c16e75109c75637f86f1f0a81a9f7e
Mohamed Achaq <motriyanox@gmail.com>
2022-06-08 14:40:13 +0100

    adding gradient animation keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,43 @@ module.exports = {
         sans: ["Generator", ...defaultTheme.fontFamily.sans],
       },
     },
+    animation: {
+      "gradient-x": "gradient-x 3s ease infinite",
+      "gradient-y": "gradient-y 3s ease infinite",
+      "gradient-xy": "gradient-xy 3s ease infinite",
+    },
+    keyframes: {
+      "gradient-y": {
+        "0%, 100%": {
+          "background-size": "400% 400%",
+          "background-position": "center top",
+        },
+        "50%": {
+          "background-size": "200% 200%",
+          "background-position": "center center",
+        },
+      },
+      "gradient-x": {
+        "0%, 100%": {
+          "background-size": "200% 200%",
+          "background-position": "left center",
+        },
+        "50%": {
+          "background-size": "200% 200%",
+          "background-position": "right center",
+        },
+      },
+      "gradient-xy": {
+        "0%, 100%": {
+          "background-size": "400% 400%",
+          "background-position": "left center",
+        },
+        "50%": {
+          "background-size": "200% 200%",
+          "background-position": "right center",
+        },
+      },
+    },
   },
   plugins: [
     plugin(function ({ addUtilities }) {
