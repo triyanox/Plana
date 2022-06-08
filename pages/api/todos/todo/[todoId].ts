c638634c59e95f69c26827c419ee5b1e80c0d30d
Mohamed Achaq <motriyanox@gmail.com>
2022-06-08 05:42:40 +0100

    new todo route

--- pages/api/todos/todo/[todoId].ts
+++ pages/api/todos/todo/[todoId].ts
@@ -21,14 +21,39 @@ export default async function handler(
     if (!isExist) {
       return res.status(400).json({ error: "user not found" });
     }
-    const { id } = req.query;
+    const { todoId } = req.query;
     const { status } = req.body;
     const todo = await prisma.todo.update({
       where: {
-        id: Number(id),
+        id: Number(todoId),
       },
       data: {
         status,
+      },
+    });
+    if (!todo) {
+      return res.status(400).json({ error: "todo not found" });
+    }
+    res.status(200).json(todo);
+  }
+  if (req.method === "DELETE") {
+    const token = req.cookies.token;
+    if (!token)
+      return res.status(401).send("Access denied. No token provided.");
+    const user: any = jwt.verify(token, env.JWT_SECRET as string);
+    if (!user) return res.status(401).send("Access denied.");
+    const isExist = await prisma.user.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+    if (!isExist) {
+      return res.status(400).json({ error: "user not found" });
+    }
+    const { todoId } = req.query;
+    const todo = await prisma.todo.delete({
+      where: {
+        id: Number(todoId),
       },
     });
     if (!todo) {
