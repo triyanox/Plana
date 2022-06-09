c59c5d08deb5aabe247b53a2236e526e272dd516
Mohamed Achaq <motriyanox@gmail.com>
2022-06-09 04:49:38 +0100

    fixing the imports

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,53 +1,55 @@
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { NextApiRequest } from "next";
+
 import * as jose from "jose";
 import { env } from "process";
 
-export default function middleware(req: NextApiRequest) {
+export default function middleware(req: NextRequest) {
   const jwt_Secret = env.JWT_SECRET;
   const token = req.cookies.token;
   const url = req.url;
+  if (url) {
+    if (url.includes("/login")) {
+      if (token) {
+        try {
+          jose.jwtVerify(token, new TextEncoder().encode(jwt_Secret));
+          return NextResponse.redirect("/dashboard");
+        } catch (err) {
+          return NextResponse.next();
+        }
+      }
+    }
+    if (url.includes("/signup")) {
+      if (token) {
+        try {
+          jose.jwtVerify(token, new TextEncoder().encode(jwt_Secret));
+          return NextResponse.redirect("/dashboard");
+        } catch (err) {
+          return NextResponse.next();
+        }
+      }
+    }
+    if (url.includes("/")) {
+      if (token) {
+        try {
+          jose.jwtVerify(token, new TextEncoder().encode(jwt_Secret));
+          return NextResponse.redirect("/dashboard");
+        } catch (err) {
+          return NextResponse.next();
+        }
+      }
+    }
 
-  if (url && url.includes("/login")) {
-    if (token) {
+    if (url.includes("/dashboard")) {
+      if (!token) {
+        return NextResponse.redirect("/login");
+      }
       try {
         jose.jwtVerify(token, new TextEncoder().encode(jwt_Secret));
-        return NextResponse.redirect("/dashboard");
-      } catch (err) {
         return NextResponse.next();
-      }
-    }
-  }
-  if (url && url.includes("/signup")) {
-    if (token) {
-      try {
-        jose.jwtVerify(token, new TextEncoder().encode(jwt_Secret));
-        return NextResponse.redirect("/dashboard");
       } catch (err) {
-        return NextResponse.next();
+        return NextResponse.redirect("/login");
       }
-    }
-  }
-  if (url && url.includes("/")) {
-    if (token) {
-      try {
-        jose.jwtVerify(token, new TextEncoder().encode(jwt_Secret));
-        return NextResponse.redirect("/dashboard");
-      } catch (err) {
-        return NextResponse.next();
-      }
-    }
-  }
-
-  if (url && url.includes("/dashboard")) {
-    if (!token) {
-      return NextResponse.redirect("/login");
-    }
-    try {
-      jose.jwtVerify(token, new TextEncoder().encode(jwt_Secret));
-      return NextResponse.next();
-    } catch (err) {
-      return NextResponse.redirect("/login");
     }
   }
 
