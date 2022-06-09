c89e6ad8c8f909aa03bee838ae786a9594d52567
Mohamed Achaq <motriyanox@gmail.com>
2022-06-09 03:44:25 +0100

    more padding arrount content on home

--- components/ui/Features.tsx
+++ components/ui/Features.tsx
@@ -2,7 +2,7 @@ import GetThingsDone from "./GetThingsDone";
 import StayFocused from "./StayFocused";
 const Features = () => {
   return (
-    <section className="w-full pt-24 lg:pt-48 px-4 sm:px-8 md:px-12 flex flex-col items-center justify-center gap-8">
+    <section className="w-full pt-24 lg:pt-48 px-6 sm:px-8 md:px-12 flex flex-col items-center justify-center gap-8">
       <GetThingsDone />
       <StayFocused />
     </section>
