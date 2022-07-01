5796501949e767981625a5e62a41c22927a41e55
Mohamed Achaq <motriyanox@gmail.com>
2022-07-01 10:03:19 +0100

    Update Footer.tsx

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -4,7 +4,7 @@ import { IconButton } from "./Buttons";
 
 const Footer = () => {
   return (
-    <footer className="w-full mt-24 pb-8 flex flex-col md:flex-row px-6 sm:px-8 md:px-12 py-4 justify-between items-center">
+    <footer className="w-full mt-24 pb-8 flex flex-row px-6 sm:px-8 md:px-12 py-4 justify-between items-center">
       <div className="flex w-full justify-center items-start flex-col gap-2">
         <h1 className="text-2xl font-bold text-black dark:text-white">Plana</h1>
         <h2 className="text-xl text-black dark:text-white">
